1fc830e80216c5e211c9894714532a05a445285d
geoffdgeorge <geoffdgeorge@users.noreply.github.com>
2019-03-08 22:07:58 -0600

    Players can now be chosen. A small victory.

--- site.js
+++ site.js
@@ -8,16 +8,18 @@ $(document).ready(function() {
         projectId: "brieftest-300a4",
         storageBucket: "brieftest-300a4.appspot.com",
         messagingSenderId: "704522530238"
-      };
+    };
     firebase.initializeApp(config);
 
     const database = firebase.database();
 
     /* Initial Manipulable Variables */
     let user;
-    let userKey
+    let userKey;
     let player;
+    let playerKey;
     let opponent;
+    let spotsFilled = 0;
     let gameInProgress = false;
 
     /* HTML Variables */
@@ -26,24 +28,26 @@ $(document).ready(function() {
     const player1Name = $('#player-one-name');
     const player2Name = $('#player-two-name');
     const p1ImgArea = $('#player-one-throw');
-    const p2ImgArea = $('#player-two-throw')
+    const p2ImgArea = $('#player-two-throw');
     const rP1Btn = $('#rP1');
     const pP1Btn = $('#pP1');
     const sP1Btn = $('#sP1');
     const rP2Btn = $('#rP2');
     const pP2Btn = $('#pP2');
     const sP2Btn = $('#sP2');
-    const resultsArea = $('#results');
+    const resultsMessage = $('#results');
     const p1Wins = $('#p1Wins');
     const p1Losses = $('#p1Losses');
     const p2Wins = $('#p2Wins');
     const p2Losses = $('#p2Losses');
     const ties = $('#ties');
-    const messageArea = $('#messages');
+    const chatWindow = $('#chat-window');
+    const chatMessage = $('#chat-message');
+    const chatSubmit = $('#chat-submit');
 
     /* Firebase Storage Variables */
     const usersRef = database.ref('/usersRef');
-    const playersRef = database.ref('/playersRef');
+    const chatsRef = database.ref('/chatsRef');
     const connectedRef = database.ref('.info/connected');
 
     /* Object of functions */
@@ -52,7 +56,7 @@ $(document).ready(function() {
         firebaseListeners: function() {
 
             /* When a user connects ... */
-            connectedRef.on("value", function(snap) {
+            connectedRef.on('value', function(snap) {
 
                 /* If they are connected ... */
                 if (snap.val()) {
@@ -62,43 +66,72 @@ $(document).ready(function() {
                         name: '',
                         wins: 0,
                         losses: 0,
+                        selection: '',
+                        joinedFirst: false,
                         playing: false,
-                        turn: false
+                        turn: false,
                     });
 
                     // Remove user from the connection list when they disconnect
                     con.onDisconnect().remove();
 
+                    userKey = con.key;
+                    
                 }
             });
-            
-            playersRef.on("value", function(snap) {
-                if(snap.numChildren < 3) {
 
-                }
+            usersRef.on('child_changed', function(snap){
+                if(spotsFilled === 0) {
+                    player = snap.val();
+                    player1Name.text(player.name);
+                    spotsFilled++;
+                } else if(spotsFilled === 1 && snap.val().joinedFirst === false) {
+                    opponent = snap.val();
+                    player2Name.text(opponent.name)
+                    spotsFilled++;
+                    if(spotsFilled > 1) {
+                        resultsMessage.text('Player 1, choose your weapon.')
+                    }
+                } 
+            })
+
+            chatsRef.on('child_added', function(snap) {
+                const messageText = snap.val();
+                const newMessage = $('<p>');
+                newMessage.text(messageText);
+                newMessage.appendTo(chatWindow);
             })
 
         },
 
         userJoin: function() {
             event.preventDefault();
-            if(!gameInProgress) {
+            if(!gameInProgress && spotsFilled === 0) {
                 name = submitField.val().trim();
-                const newPlayer = {
+                usersRef.child(userKey).update({
                     name: name,
-                    wins: 0,
-                    losses: 0,
-                    playing: false,
-                    turn: false
-                }
-                playersRef.push(newPlayer);
-
+                    playing: true,
+                    joinedFirst: true
+                });
+            } else if(!gameInProgress && spotsFilled === 1) {
+                name = submitField.val().trim();
+                usersRef.child(userKey).update({
+                    name: name,
+                    playing: true
+                });
             }
         },
 
+        chatAdd: function() {
+            event.preventDefault();
+            const message = chatMessage.val().trim();
+            chatsRef.push(message);
+        }
+    
     }
     
     submitBtn.click(rochambeau.userJoin);
+    chatSubmit.click(rochambeau.chatAdd);
     rochambeau.firebaseListeners();
 })
 
